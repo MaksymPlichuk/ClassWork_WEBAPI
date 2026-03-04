47dacb9f4343de7daff3fbdc8c7d28cc49626ee1
MaksymPlichuk <pilipm449@gmail.com>
2026-03-04 17:53:06 +0200

    Seeder to/do

---
+++ ClassWork_WEBAPI.DLL/Entities/AuthorEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassWork_WEBAPI.DLL.Entities
+{
+    public class AuthorEntity : BaseEntity
+    {
+        public required string Name { get; set; }
+        public DateTime BirthDate { get; set; } = DateTime.UtcNow;
+        public string? Image { get; set; }
+        public List<BookEntity> Books { get; set; } = [];
+    }
+}
---
+++ ClassWork_WEBAPI.DLL/Entities/BaseEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassWork_WEBAPI.DLL.Entities
+{
+    public interface IBaseEntiy
+    {
+        int Id { get; set; }
+        DateTime CreateDate { get; set; }
+    }
+
+
+    public class BaseEntity : IBaseEntiy
+    {
+        public int Id { get; set ; }
+        public DateTime CreateDate { get; set; } = DateTime.UtcNow;
+    }
+}
