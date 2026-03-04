c6a4e54b19c920cd5094be1461e2318368dc01ff
Maksym <pilipm449@gmail.com>
2026-03-04 12:14:51 +0200

    Add project files.

---
+++ ClassWork_WEBAPI.API/Program.cs
@@ -0,0 +1,33 @@
+using ClassWork_WEBAPI.DLL;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    string? connectionString = builder.Configuration.GetConnectionString("LocalDB");
+    options.UseNpgsql(connectionString);
+});
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ ClassWork_WEBAPI.DLL/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassWork_WEBAPI.DLL
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+    }
+}
